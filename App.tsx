96fb8de4dedfef37bf2c99e1c5744d523d4ce5fb
RodrigoGSantos <rodrigo.guantos@gmail.com>
2022-11-03 21:01:10 -0300

    first-commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from './src/components/Loading'
+import { SingIn } from './src/screens/SingIn';
+
+
+export default function App() {
+  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontLoaded ? <SingIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
